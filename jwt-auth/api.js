e5f8e3c9dd4d6749ef464b56ba32bab8c848e640
github username <github email address>
2020-07-05 17:34:23 -0500

    test remote branch

--- jwt-auth/api.js
+++ jwt-auth/api.js
@@ -8,20 +8,22 @@ const jwtCheck = expressjwt({
   secret: "mykey"
 });
 
+
 app.use(bodyParser.json());
 
 app.listen(PORT, () => {
   console.log(`Serving is running on port ${PORT}.`);
 });
-
+// anyone can see this route, as its public
 app.get('/asset', (req, res) => {
   res.status(200).send("Everybody can see this");
 });
 
+//checking to see if signature that was provided to user after providing credentials matches
 app.get("/asset/secret", jwtCheck, (req, res) => {
-  res.status(200).send("Only logged in people can see me");
+  res.status(200).send(`Only logged in people can see me !`);
 });
-
+//catchall route
 app.get("*", (req, res)=> {
   res.sendStatus(404);
 });
